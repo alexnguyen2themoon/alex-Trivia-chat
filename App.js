72527e4ad7e99b659be30a01e67b58e272b21177
Alex Nguyen <anguyen10@Alexs-MacBook-Pro.local>
2026-07-09 13:43:11 -0700

    made my questions array objects for trivia and gamestate function

--- App.js
+++ App.js
@@ -8,8 +8,27 @@ const CHATBOT_USER_OBJ = {
   avatar: "https://loremflickr.com/140/140",
 };
 
+//My questions
+
+const trivia_questions_alex = [
+  {
+    question: "What's my favorite color?\n 1) Purple\n2)B) Green\n3) Blue",
+    answer: "3"
+  },
+  {
+    question: "What language can I speak?\n1) Japanese\n2) Vietnamese\n 3) Spanish",
+    answer: "2"
+  },
+  {
+    question: "What's my favorite food?\n1) Pho Noodles\n2)2) Birria Taco\n 3) Chicken Katsu",
+    answer: "1"
+  }
+];
+
 export default function App() {
   const [messages, setMessages] = useState([]);
+  const [gameState, setGateState] = useState(-1); //Google says useState should be -1 bc it's the lobby / welcome screen (waiting for "Yes").
+
 
 
   useEffect(() => {
@@ -23,8 +42,8 @@ export default function App() {
 
   const addNewMessage = (newMessages) => {
     setMessages((previousMessages) => {
-      // console.log("PREVIOUS MESSAGES:", previousMessages);
-      // console.log("NEW MESSAGE:", newMessages);
+      console.log("PREVIOUS MESSAGES:", previousMessages);
+      console.log("NEW MESSAGE:", newMessages);
       return GiftedChat.append(previousMessages, newMessages);
     });
   };
@@ -40,10 +59,25 @@ export default function App() {
     ]);
   };
 
+    const addRemindMessage = (text) => {
+    addNewMessage([
+      {
+        _id: Math.round(Math.random() * 1000000),
+        text: text,
+        createdAt: new Date(),
+        user: CHATBOT_USER_OBJ,
+      },
+    ]);
+  };
+
+
   const respondToUser = (userMessages) => {
     console.log("Recent user msg:", userMessages[0].text);
-
-    // addBotMessage("I am da response!");
+      if (userMessages == "Yes"){
+        addBotMessage("I am da response!");
+      } else {
+        addRemindMessage("Please say \'Yes\'");
+      }  
   };
 
   const onSend = useCallback((messages = []) => {
